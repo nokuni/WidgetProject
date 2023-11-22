dc6648ab3e3d5d563ca68affa6091fe26d377fb9
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-11-22 13:56:45 +0100

    Initial Commit

---
+++ WidgetProject/WidgetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetProjectApp.swift
+//  WidgetProject
+//
+//  Created by Yann Christophe Maertens on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
